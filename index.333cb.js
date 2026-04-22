f0322f63fcd65765514680b8f28b425e32e08fef
tellerYami <>
2026-04-22 22:14:04 +0800

    修复进度条错位

--- index.333cb.js
+++ index.333cb.js
@@ -1,4 +1,4 @@
-System.register(["./application.ab0bc.js"], function (_export, _context) {
+System.register(["./application.59789.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
