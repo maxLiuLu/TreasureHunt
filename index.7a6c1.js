193967e760178cab68748cc2053909af415eca25
tellerYami <>
2026-04-22 22:16:36 +0800

    1

--- index.7a6c1.js
+++ index.7a6c1.js
@@ -1,4 +1,4 @@
-System.register(["./application.b5525.js"], function (_export, _context) {
+System.register(["./application.cdb4d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
