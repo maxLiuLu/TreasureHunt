8c2752a2611ab01082d07a4154d350b1b5072e58
tellerYami <>
2026-04-23 16:46:18 +0800

    1

--- application.759de.js
+++ application.759de.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.4d101.json';
+          this.settingsPath = 'src/settings.e0e01.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
