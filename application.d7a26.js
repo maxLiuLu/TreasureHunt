24cad105f56df31509ee8ac0e1a8918f2e515631
tellerYami <>
2026-04-24 17:53:45 +0800

    1

--- application.d7a26.js
+++ application.d7a26.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.a1a00.json';
+          this.settingsPath = 'src/settings.ea7bf.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
