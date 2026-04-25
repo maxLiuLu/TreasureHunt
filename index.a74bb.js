b96b464cb0ee7ff9d4f2bc73262384f57de624e4
tellerYami <>
2026-04-25 22:23:40 +0800

    1

--- index.a74bb.js
+++ index.a74bb.js
@@ -1,4 +1,4 @@
-System.register(["./application.8c4a3.js"], function (_export, _context) {
+System.register(["./application.23abc.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
