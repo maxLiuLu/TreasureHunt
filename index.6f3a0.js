59eb42ac1d8ebbb0953cd3c94d1b59d885b7638e
tellerYami <>
2026-04-25 15:10:28 +0800

    1.0.0

--- index.6f3a0.js
+++ index.6f3a0.js
@@ -1,4 +1,4 @@
-System.register(["./application.25986.js"], function (_export, _context) {
+System.register(["./application.8c4a3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
