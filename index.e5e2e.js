ebf1e5761bcaf14d24a8c43690372c55bd9af8cf
tellerYami <>
2026-04-25 12:51:11 +0800

    1

--- index.e5e2e.js
+++ index.e5e2e.js
@@ -1,4 +1,4 @@
-System.register(["./application.70ed6.js"], function (_export, _context) {
+System.register(["./application.70139.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
