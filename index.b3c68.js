23e02b0b4a8aa4d762e6c48ae21763fb6288808d
tellerYami <>
2026-04-26 01:46:07 +0800

    1

--- index.b3c68.js
+++ index.b3c68.js
@@ -1,4 +1,4 @@
-System.register(["./application.23abc.js"], function (_export, _context) {
+System.register(["./application.35cdd.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
