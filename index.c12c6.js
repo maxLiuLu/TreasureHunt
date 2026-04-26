1cf0cb170c3e98dbb25913579879c8d1d7fa34ba
tellerYami <>
2026-04-26 23:43:52 +0800

    1

--- index.c12c6.js
+++ index.c12c6.js
@@ -1,4 +1,4 @@
-System.register(["./application.5a9ac.js"], function (_export, _context) {
+System.register(["./application.62993.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
